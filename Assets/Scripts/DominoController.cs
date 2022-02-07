81b1e27cfd408e540796593733a5e9efcf35d34d
Ekaterina Litskevich <98738042+EkaterinaLitskevich@users.noreply.github.com>
2022-02-07 17:44:02 +0300

    Add prefab domino and half domino

---
+++ Assets/Scripts/DominoController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DominoController : MonoBehaviour
+{
+    private List<Half> _halfs = new List<Half>();
+    
+
+}
---
+++ Assets/Scripts/Half.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+
+public class Half : MonoBehaviour
+{
+    [SerializeField] private Image _image;
+    
+    private int _value;
+}
