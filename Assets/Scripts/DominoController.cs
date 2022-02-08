82d828564cc79446f42c76737aa3233adb831c88
Ekaterina Litskevich <98738042+EkaterinaLitskevich@users.noreply.github.com>
2022-02-08 11:16:02 +0300

    Add half grid fill

--- Assets/Scripts/DominoController.cs
+++ Assets/Scripts/DominoController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,53 @@ using UnityEngine.UI;
 
 public class DominoController : MonoBehaviour
 {
-    private List<Half> _halfs = new List<Half>();
+    [SerializeField] private int _amountHalfs;
+    [SerializeField] private Half _halfPrefab;
     
+    private List<Half> _halfs = new List<Half>();
+    private List<Half> _halfForCompare = new List<Half>();
 
+    private bool _isStand;
+
+    private void Start()
+    {
+        FillArray();
+        FillGrid();
+    }
+
+    private void FillArray()
+    {
+        Half half = new Half();
+        
+        for (int i = 0; i < _amountHalfs; i++)
+        {
+            half = Instantiate(_halfPrefab, transform.position, transform.rotation, null);
+            _halfs.Add(half);
+        }
+    }
+
+    private void FillGrid()
+    {
+        int halfArray = _amountHalfs / 2;
+
+        for (int i = 0; i < halfArray; i++)
+        {
+            _halfs[i].transform.SetParent(transform);
+            SetHalfsForCompare(true, i);
+        }
+
+        for (int i = _halfs.Count - 1; i >= halfArray; i--)
+        {
+            _halfs[i].transform.SetParent(transform);
+            SetHalfsForCompare(false, i);
+        }
+    }
+
+    private void SetHalfsForCompare(bool isStand, int i)
+    {
+        if (_isStand == isStand)
+        {
+            _halfForCompare.Add(_halfs[i]);
+        }
+    }
 }
