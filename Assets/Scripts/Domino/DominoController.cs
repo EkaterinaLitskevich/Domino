a77adc1870ff0c219538ae450a80996899410e73
Ekaterina Litskevich <98738042+EkaterinaLitskevich@users.noreply.github.com>
2022-02-08 17:19:37 +0300

    Add domino rotate (not complete)

--- Assets/Scripts/Domino/DominoController.cs
+++ Assets/Scripts/Domino/DominoController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,6 +6,8 @@ namespace Domino
 {
     public class DominoController : MonoBehaviour
     {
+        private const int ValueRotateZ = 180;
+        
         [SerializeField] private Half _halfPrefab;
         [SerializeField] private BoxCollider2D _collider;
         [SerializeField] private int _amountHalfs;
@@ -12,10 +15,13 @@ namespace Domino
         private List<Half> _halfs = new List<Half>();
         private List<Half> _halfForCompare = new List<Half>();
 
+        private int _halfArray;
         public bool IsStand { get; set; }
 
         private void Start()
         {
+            _halfArray = _amountHalfs / 2;
+            
             FillArray();
             FillGrid();
             SetCollider();
@@ -28,21 +34,20 @@ namespace Domino
             for (int i = 0; i < _amountHalfs; i++)
             {
                 half = Instantiate(_halfPrefab, transform.position, transform.rotation, null);
+                half.name = "Half " + i;
                 _halfs.Add(half);
             }
         }
 
         private void FillGrid()
         {
-            int halfArray = _amountHalfs / 2;
-
-            for (int i = 0; i < halfArray; i++)
+            for (int i = 0; i < _halfArray; i++)
             {
                 _halfs[i].transform.SetParent(transform);
                 SetHalfsForCompare(true, i);
             }
 
-            for (int i = _halfs.Count - 1; i >= halfArray; i--)
+            for (int i = _halfs.Count - 1; i >= _halfArray; i--)
             {
                 _halfs[i].transform.SetParent(transform);
                 SetHalfsForCompare(false, i);
@@ -65,7 +70,78 @@ namespace Domino
         
         private void Rotate()
         {
+            transform.Rotate(0,0,ValueRotateZ);
+            SetElementForArray();
+        }
+
+        private void SetElementForArray()
+        {
+            foreach (var t in _halfs)
+            {
+                Debug.Log("elevent: " + t);
+            }
             
+            Half half;
+
+            if (_halfArray > 2)
+            {
+                for (int i = _halfs.Count - 1; i >= _halfArray; i--) //array in reverse order
+                {
+                    half = _halfs[i];
+
+                    _halfs[i] = _halfs[_halfs.Count - i - 1];
+                    _halfs[_halfs.Count - i - 1] = half;
+                }
+
+                foreach (var t in _halfs)
+                {
+                    Debug.Log("elevent: " + t);
+                }
+                int index = 0;
+                for (int i = _halfs.Count - 1; i >= _halfArray; i--) //bottom numbers in reverse order
+                {
+                    half = _halfs[i];
+                    _halfs[i] = _halfs[_halfArray + index];
+                    _halfs[_halfArray + index] = half;
+                    index++;
+                }
+                
+  
+                
+                /*for (int i = 0; i < _halfArray / 2; i++)
+                {
+                    Debug.Log("_halfArray / 2 = " + _halfArray / 2);
+                    Debug.Log("i = " + i);
+                    
+                    half = _halfs[i];
+
+                    _halfs[i] = _halfs[_halfArray - i];
+                    _halfs[_halfArray - i] = half;
+                }*/
+            }
+            else
+            {
+                for (int i = 0; i < _halfArray; i++)
+                {
+                    half = _halfs[i];
+
+                    _halfs[i] = _halfs[_halfArray - i];
+                    _halfs[_halfArray - i] = half;
+                }
+            }
+
+            foreach (var t in _halfs)
+            {
+                Debug.Log("elevent: " + t);
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Rotate();
+            }
         }
     }
 }
