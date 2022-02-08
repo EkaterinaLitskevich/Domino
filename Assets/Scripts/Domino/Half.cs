8cd354f371295b7757fd48f813d29167abf176ec
Ekaterina Litskevich <98738042+EkaterinaLitskevich@users.noreply.github.com>
2022-02-08 11:52:22 +0300

    Add random value half

---
+++ Assets/Scripts/Domino/Half.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+using Random = System.Random;
+
+public class Half : MonoBehaviour
+{
+    private const int MaxValue = 6;
+    private const int MinValue = 0;
+    
+    [SerializeField] private Image _image;
+    
+    private int _value;
+
+    private void Start()
+    {
+        SetValue();
+        SetImage();
+    }
+
+    private void SetValue()
+    {
+        Random random = new Random();
+
+        _value = random.Next(MinValue, MaxValue + 1);
+    }
+
+    private void SetImage()
+    {
+    }
+}
